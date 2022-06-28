9b6e38b37c4cd421cb1a96e55a43d87b1cfcfdc9
Andrew Maurer <andmaurer13@gmail.com>
2022-06-27 22:05:29 -0400

    added some js, import export, pics and landing page

---
+++ js/execution.js
@@ -0,0 +1,8 @@
+import {deployHeader} from "./headerComponent.js";
+
+// const indexExecution = document.querySelector("#homeButton");
+
+// indexExecution.addEventListener("click", deployHeader());
+
+
+window.addEventListener('load', deployHeader());
---
+++ js/headerComponent.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+function deployHeader (){
+    const headerItem = 
+`
+<img class="mainPhoto" src="../src/images/gma and gpa sign.jpg">
+<header class="mainHeader">
+<h2 class="title">Gaf-Fine Farms</h2>
+<h4 class="subtitle">Family owned and operated since 1904</h4>
+</header>`;
+document.getElementById("mainHeader").innerHTML = headerItem;
+
+}
+
+
+export {deployHeader};
+
+
+
+
+
+
+
+
