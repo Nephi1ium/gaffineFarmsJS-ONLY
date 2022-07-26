f23b6df7f234add8de2fbef0ab83ed5764507933
Andrew Maurer <andmaurer13@gmail.com>
2022-07-25 22:15:06 -0400

    added some images

--- js/array/imagegallery.js
+++ js/array/imagegallery.js
@@ -1,7 +1,40 @@
 const imageGallery = [
     '../src/images/3gengaffnermen.jpg', 
     
-    '../src/images/abby_gaffner.jpg'
+    '../src/images/abby_gaffner.jpg',
+
+    '../src/images/gaffner girls dutchesses.jpg',
+
+    '../src/images/gma and gpa sign.jpg',
+
+    '../src/images/jerry_gaffner.jpg',
+
+    '../src/images/scott_gaffner.jpg',
+
+    '../src/images/sg_cornfield.jpg',
+
+    '../src/images/sg_cw_sign.jpg',
+
+    '../src/images/Family Triangle Florida.jpg',
+
+    '../src/images/jonah and quinn post engagement.jpg',
+
+    '../src/images/jonah and quinn.jpg',
+
+    '../src/images/jonah mountains.jpg',
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
